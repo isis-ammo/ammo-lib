72d2325462aeace571e8ce48696ce3bfcd30c876
Demetri Miller <demetri.miller@gmail.com>
2011-02-18 21:46:18 -0600

    Refacored AmmoCore UI for preference changes. Commit before repository change

---
+++ AmmoLib/src/edu/vu/isis/ammo/AmmoPrefKeys.java
@@ -0,0 +1,18 @@
+package edu.vu.isis.ammo;
+
+/**
+ * Collection of all preference values used by Ammo.
+ * @author Demetri Miller
+ *
+ */
+public final class AmmoPrefKeys {
+	// ====================================
+	// Ammo Core
+	// ====================================
+	public static final String PHYSICAL_LINK_PREF_STATUS_KEY = "edu.vu.isis.ammo.core.physical_link.STATUS.KEY";
+	public static final String WIFI_PREF_STATUS_KEY = "edu.vu.isis.ammo.core.wifi_link.STATUS.KEY";
+	public static final String PHYSICAL_LINK_SHOULD_USE_PREF_KEY = "edu.vu.isis.ammo.core.physical_link.SHOULD_USE.KEY";
+	public static final String WIFI_SHOULD_USE_PREF_KEY = "edu.vu.isis.ammo.core.wifi_link.SHOULD_USE.KEY";
+	
+
+}
