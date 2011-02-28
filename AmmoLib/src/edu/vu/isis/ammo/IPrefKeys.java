39d25c6526d0fea5051afc87d8d225d7c90f141e
Fred Eisele <feisele@isis.vanderbilt.edu>
2011-02-28 11:45:12 -0600

    resolving inconsistencies with pref keys

---
+++ AmmoLib/src/edu/vu/isis/ammo/IPrefKeys.java
@@ -0,0 +1,10 @@
+package edu.vu.isis.ammo;
+
+/**
+ * This is extended in edu.vu.isis.ammo.core
+ * @author phreed
+ *
+ */
+public interface IPrefKeys {
+	public static final String PREF_OPERATOR_ID = "CORE_OPERATOR_ID";
+}
