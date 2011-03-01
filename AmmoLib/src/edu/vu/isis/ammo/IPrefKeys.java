c63a8ef6753fe8eab18a41c6a4b0dead3eb9bf0a
Demetri Miller <demetri.miller@gmail.com>
2011-03-01 16:36:55 -0600

    Added AIDL communication between QuickReport and AmmoCore for sending preferences. Removed PreferenceServiceHack. Moved INetPrefKeys to AmmoLib since mutliple applictions use it.

--- AmmoLib/src/edu/vu/isis/ammo/IPrefKeys.java
+++ AmmoLib/src/edu/vu/isis/ammo/IPrefKeys.java
@@ -9,4 +9,5 @@ public interface IPrefKeys {
 	public static final String PREF_OPERATOR_ID = "CORE_OPERATOR_ID";
 	public static final String AMMO_LOGIN = "AMMO_LOGIN";
 	public static final String AMMO_CONNECTED = "AMMO_CONNECTED";
+	public static final String AMMO_PREF_UPDATE = "edu.vu.isis.ammo.core.PREF_UPDATE";
 }
