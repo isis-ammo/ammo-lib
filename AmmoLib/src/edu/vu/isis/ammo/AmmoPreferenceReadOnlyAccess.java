8078fada7614c40989753e93fbd1439a0dba6d4b
Demetri Miller <demetri.miller@gmail.com>
2011-03-07 12:27:05 -0600

    AmmoPreferences now only modifiable by AmmoCore

---
+++ AmmoLib/src/edu/vu/isis/ammo/AmmoPreferenceReadOnlyAccess.java
@@ -0,0 +1,10 @@
+package edu.vu.isis.ammo;
+
+public class AmmoPreferenceReadOnlyAccess extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8343926238978002081L;
+
+}
