3b298aa45486577d6307dfa9849d5e5b80c37c80
Demetri Miller <demetri.miller@gmail.com>
2011-04-04 15:58:18 -0500

    Added pull handling in AmmoSMS

--- AmmoLib/src/edu/vu/isis/ammo/sms/provider/SmsSchema.java
+++ AmmoLib/src/edu/vu/isis/ammo/sms/provider/SmsSchema.java
@@ -3,23 +3,24 @@ package edu.vu.isis.ammo.sms.provider;
 
 public class SmsSchema extends SmsSchemaBase {
 
-   public static final int DATABASE_VERSION = 1;
+	public static final int DATABASE_VERSION = 1;
 
-      public static class MessageTableSchema extends MessageTableSchemaBase {
+	public static class MessageTableSchema extends MessageTableSchemaBase {
 
-         protected MessageTableSchema() { super(); }
-      /**
-        Add relation constants as appropriate.
-        i.e.
-        public static final String <NAME> = "<sponsor>.provider.<name>.<table>.action.<NAME>";
-        e.g.
-        public static final String CONSTANT = "mil.darpa.transapp.ammo.sms.provider.sms.message.action.CONSTANT";
+		protected MessageTableSchema() {
+			super();
+		}
+		/**
+		 * Add relation constants as appropriate. i.e. public static final
+		 * String <NAME> = "<sponsor>.provider.<name>.
+		 * <table>
+		 * .action.<NAME>"; e.g. public static final String CONSTANT =
+		 * "mil.darpa.transapp.ammo.sms.provider.sms.message.action.CONSTANT";
+		 * 
+		 * public static final String PRIORITY_SORT_ORDER =
+		 * MessageTableSchemaBase.EXPIRATION + " DESC, " +
+		 * MessageTableSchemaBase.MODIFIED_DATE + " DESC ";
+		 */
+	}
 
-        public static final String PRIORITY_SORT_ORDER = 
-               MessageTableSchemaBase.EXPIRATION + " DESC, " +
-               MessageTableSchemaBase.MODIFIED_DATE + " DESC ";
-      */
-      }
-
-      
 }
