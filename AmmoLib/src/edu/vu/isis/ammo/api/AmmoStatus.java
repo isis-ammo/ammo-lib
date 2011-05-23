3b385b077650498617c27c939924e12e268fc8a9
Fred Eisele <feisele@isis.vanderbilt.edu>
2011-05-23 16:16:00 -0500

    generated java API from noweb

--- AmmoLib/src/edu/vu/isis/ammo/api/AmmoStatus.java
+++ AmmoLib/src/edu/vu/isis/ammo/api/AmmoStatus.java
@@ -1,28 +1,22 @@
+// AmmoStatus.java
 package edu.vu.isis.ammo.api;
-
 /**
- * See also AmmoRequest.java IAmmoRequest.java and IAmmoPolicy.java
- */
+  See docs/java/ammo-api.nw for documentation
+*/ 
+abstract public class AmmoStatus implements IAmmoStatus {
 
-/**
- * This API supersedes the AmmoDispatcher calls.
- * Those methods will still work but they are deprecated.
- * 
- * This abstract class supplies factory methods
- * @author phreed
- *
- */
-abstract public class AmmoStatus implements IAmmoPolicy {
-	
-	public abstract static class  NetLink implements IAmmoPolicy.NetLink {
-		static public NetLink make(String name) { return null; }
-	}
+   public abstract static class  NetLink implements IAmmoStatus.NetLink {
+      static public NetLink make(String name) { return null; }
+   }
 
-	public abstract static class  Gateway implements IAmmoPolicy.Gateway {
-		static public Gateway make(String name) { return null; }	
-	}
-	
-	public abstract static class  NetworkController implements IAmmoPolicy.NetworkController {
-		static public NetworkController make(String name) { return null; }
-	}
+   public abstract static class  Gateway implements IAmmoStatus.Gateway {
+      static public Gateway make(String name) { return null; }
+   }
+
+   public abstract static class  NetworkController implements IAmmoStatus.NetworkController {
+      static public NetworkController make(String name) { return null; }
+   }
 }
+
+// not implemented
+
--- AmmoLib/src/edu/vu/isis/ammo/api/IAmmoRequest.java
+++ AmmoLib/src/edu/vu/isis/ammo/api/IAmmoRequest.java
@@ -1,20 +1,8 @@
-
+// IAmmoRequest.java
 package edu.vu.isis.ammo.api;
 /**
- * See also AmmoRequest.java IAmmoPolicy.java and AmmoPolicy.java
- * 
- * An IAmmoRequest is an immutable object.
- * They come in two main varieties: Data, and Interest.
- * Data is a request for new content to be injected into the system.
- * Interest is a request to have content delivered.
- * 
- * Requests are immutable so that when a named piece of data
- * is obtained it is known that it is correct and is not waiting
- * for additional elements. (data-flow variables)
- * 
- */
-
-
+  See docs/java/ammo-api.nw for documentation
+*/ 
 import java.util.Calendar;
 
 import javax.xml.datatype.Duration;
@@ -22,285 +10,112 @@ import javax.xml.datatype.Duration;
 import android.content.Intent;
 import android.net.Uri;
 
-/**
- * This API supersedes the AmmoDispatcher calls.
- * Those methods will still work but they are deprecated.
- * 
- * @author phreed
- *
- */
 public interface IAmmoRequest {
-	
-	/**
-	 * the operator has been authorized 
-	 */
-	public static final Intent LOGIN = new Intent("edu.vu.isis.ammo.auth.LOGIN");
-	public static final Intent LOGOUT = new Intent("edu.vu.isis.ammo.auth.LOGOUT");
-	
-	/**
-	 * the gateway event is raised when the authorized gateway changes.
-	 */
-	public static final Intent GATEWAY_EVENT = new Intent("edu.vu.isis.ammo.auth.GATEWAY");
-    
-	public interface Builder {
-		/**
-		 * duplicate (and reset?)
-		 * @return
-		 */
-		public IAmmoRequest duplicate();
-		/**
-		 * The following are factory actions which produce Ammo requests
-		 * @return 
-		 */
-		public IAmmoRequest post();
-		public IAmmoRequest directedPost(Recipient recipient);
-		public IAmmoRequest publish();
-		public IAmmoRequest subscribe();
-		public IAmmoRequest retrieve();
-		
-		/**
-		 * rather than trying to dynamically update an object.
-		 * These methods are used to replace an existing request.
-		 */
-		public IAmmoRequest replace(String uuid);
-		public IAmmoRequest replace(IAmmoRequest req);
-		
-		/**
-		 * recover a previously built request, by name
-		 * @param uuid
-		 * @return
-		 */
-		public IAmmoRequest recover(String uuid);
-		
-		/**
-		 * The following parameters are known by the builder.
-		*/
-		/**
-		 * reset all settings to default values.
-		 */
-		public Builder reset();
-		/**
-		 * The content provider identifier, may also contain a specific row.
-		 * @param val
-		 * @return
-		 */
-		public Builder provider(Uri val);
-		/**
-		 * may be used in lieu of provider.
-		 * @param val
-		 * @return
-		 */
-		public Builder payload(String val);
-		
-		/**
-		 * The type of the data being injected.
-		 * 
-		 * @param val
-		 * @return
-		 */
-		public Builder type(String val);
-		
-		/**
-		 * The unique name of the data being injected.
-		 * The name only need be unique in the context of the type.
-		 * 
-		 * @param val
-		 * @return
-		 */
-		public Builder name(String val);
-		
-		/**
-		 * How long will the data item persist.
-		 * 
-		 * from the time the request is posted.
-		 * @param val
-         * @return
-		 */
+   public interface Builder {
+      public IAmmoRequest duplicate();
+
+      public Builder reset();
+
+      public IAmmoRequest post();
+      public IAmmoRequest directedPost(Recipient recipient);
+      public IAmmoRequest publish();
+      public IAmmoRequest subscribe();
+      public IAmmoRequest retrieve();
+      public IAmmoRequest interest();
+      public IAmmoRequest replace(IAmmoRequest req);
+      public IAmmoRequest replace(String uuid);
+
+      public IAmmoRequest recover(String uuid);
+
+        public Builder provider(Uri val);
+        public Builder payload(String val);
+        public Builder type(String val);
+        public Builder name(String val);
+        public Builder filter(Filter val); 
+        public Builder query(Query val);
+        public Builder downsample(Downsample val); 
+        public Builder downsample(char val); 
+
+
         public Builder longevity(Duration val);
-        /**
-         * absolute time.
-         * @param val
-         * @return
-         */
-		public Builder longevity(Calendar val);
-		/**
-		 * 0 : normal, 
-		 * >0 : higher priority, (these are sent first)
-		 * <0 : lower priority
-		 * @param val
-		 * @return
-		 */
-		public Builder priority(int val);
-		/**
-		 * notifications to be sent: what, when and why
-		 * @param val
-		 * @return
-		 */
-		public Builder notify(Notice[] val);
-		public Builder notify(Notice val);
-		
-		public Builder liveness();
-        /**
-         * from what time to you want data
-         * @param val
-         * @return
-         */
-		public Builder start(Calendar val); 
-		/**
-		 * acts as a check against priority (does not effect request delivery, but status
-		 * @param val
-		 * @return
-		 */
-		public Builder worth(int val);
-		/**
-		 * max number of gateway hops, <= 0 unlimited, generally 0 or 1
-		 * @param val
-		 * @return
-		 */
-		public Builder reach(int val);
-		/**
-		 * who will get this request?
-		 * @param val
-		 * @return
-		 */
-		public Builder recipient(Recipient val);
-		
-		/**
-		 * The maximum number of bits per second.
-		 * @param val
-		 * @return
-		 */
-		public Builder maxTransmissionRate(int val);
-		
-		/**
-		 * various filtering mechanisms
-		 */
-		public Builder filter(Filter val); 
-		public Builder query(Query val);
-		public Builder downsample(Downsample val); 
-		   
-		/**
-		   quality of service:
-		 */
-		
-	}
+        public Builder longevity(Calendar val);
 
-	/**
-	 * rejected is mutable because although one target rejects a task another may accept
-	 */
+        public Builder priority(int val);
 
-	public enum Event {
-		QUEUE , // placed under the control of the distributor
-		DISTRIBUTE, // placed under the control of a gateway
-		DELIVER, // obtained by at least one of the targets
-		COMPLETE, // a target has completed handling the task
-	}
-	
-	public enum Status {
-		SUCCESS,  // a final status
-		FAIL,     // a final status
-		UNKNOWN,  // a mutable status
-		REJECTED, // a mutable status 
-	}
-	
-	/** 
-	 * A record of the current state of the object
-	 */
-	public interface State {
-		public Event getEvent();
-		public State setEvent(Event val);
-		
-		public Status getStatus();
-		public State setStatus(Status val);
-	}
-	
-	/**
-	 * When a change is made from one state to another.
-	 * A null state indicates any.
-	 * The action 
-	 */
-	public interface Notice {
-		public State getTarget();
-		public State setTarget(State val);
-		public State getSource();
-		public State setSource(State val);
-		
-		public boolean runAction();
-		public Object getAction();
-	}
-	
-	
-	/**
-	 * Works in conjunction with the contact manager.
-	 */
-	public interface Recipient {
-		public String getCallSign();
-		public String[] getGroups();
-		public String getName(String type); // used e.g. tigr
-		public String getName(); // canonical name
-	}
-	
-	public interface Filter {
-		public Filter get();
-	}
-	
-	public interface Query {
-		public String[] getProjection();
-		public String getSelection();
-		public String[] getArgs();
-		public String[] getGroupBy();
-		public String[] getOrderBy();
-	}
-	
-	/**
-	 * An object can be down sampled to either a maximum size
-	 * or a fraction of its original size.
-	 */
-	public interface Downsample {
-		public int getMaxSize();
-		public double getFraction();
-	}
-	
-	// ********** Request control methods ***************
-	/**
-	 * get the current state of the request
-	 */
-	public State[] getState(); 
-	/**
-	 * cancel the request
-	 * @return
-	 */
-	public State[] cancel(); 
-	/**
-	 *  change the expiration (longevity) of the request
-	 * @param val
-	 * @return
-	 */
-	public State[] expiration(Duration val);
-	/**
-	 * This is provided to work in conjunction with AmmoRequest.make(uuid);
-	 * It is used to preserve references to AmmoRequest objects across a restart
-	 * @return
-	 */
-	public String getUuid(); // 
-	
-	/**
-	 * Sets the timespan used to measure various metrics.
-	 * e.g. transmission rate
-	 */
-	public void setMetricTimespan(int val);
-	public int getTransmissionRate();
-	
-	/**
-	 * When was the last message received.
-	 */
-	public Calendar getLastMessage();
-	
-	/**
-	 * Reset all statistics, counters, averages, but not time span
-	 * @param val
-	 */
-	public void resetMetrics(int val);
-	public int getTotalMessages();
-	 
+        public Builder worth(int val);
+
+              
+        public Builder liveness(int val);
+        public Builder start(Calendar val); 
+        public Builder scope(int val);
+        public Builder recipient(Recipient val);
+        public Builder maxTransmissionRate(int val);
+
+              
+
+      public interface Recipient {
+         public String getCallSign();
+         public String[] getGroups();
+         public String getName(String type); // used e.g. tigr
+         public String getName(); // canonical name
+      }
+      public interface Filter {
+         public Filter get();
+      }
+         
+         
+      public interface Query {
+         public String[] getProjection();
+         public String getSelection();
+         public String[] getArgs();
+         public String[] getGroupBy();
+         public String[] getOrderBy();
+      }
+
+      public interface Downsample {
+         public int getMaxSize();
+         public double getFraction();
+      }
+
+      public enum Place { QUEUE , DISTRIBUTE, DELIVER, COMPLETE }
+         
+      public enum Color { SUCCESS, FAIL,  UNKNOWN, REJECTED };
+         
+      public interface Event {
+         public Place getPlace();
+         public Event setPlace(Place val);
+         
+         public Color getColor();
+         public Event setColor(Color val);
+      }
+         
+      public interface Notice {
+         public Event getTarget();
+         public Event setTarget(Event val);
+         public Event getSource();
+         public Event setSource(Event val);
+            
+         public boolean runAction();
+         public Object getAction();
+      }
+
+         public Event[] getEvent(); 
+
+         public Event[] cancel(); 
+
+         public Event[] expiration(Duration val);
+
+         public String getUuid(); // 
+         
+         public void setMetricTimespan(int val);
+         public int getTransmissionRate();
+         
+         public Calendar getLastMessage();
+         
+         public void resetMetrics(int val);
+         public int getTotalMessages();
+
+   }
+
 }
+   
