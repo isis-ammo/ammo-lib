8cc94f22beb580c826f7883632e8a692f8b330e2
Mike James <mcj@isis.vanderbilt.edu>
2011-06-07 14:10:55 -0500

    Reorg of netlink code. refs #272

--- AmmoLib/src/edu/vu/isis/ammo/api/AmmoIntents.java
+++ AmmoLib/src/edu/vu/isis/ammo/api/AmmoIntents.java
@@ -4,6 +4,7 @@ public interface AmmoIntents {
 	public static final String AMMO_ACTION_ETHER_LINK_CHANGE = "edu.vu.isis.ACTION_ETHER_LINK_CHANGE";
 	public static final String AMMO_ACTION_WIFI_LINK_CHANGE = "edu.vu.isis.ACTION_WIFI_LINK_CHANGE";
 	public static final String AMMO_ACTION_GATEWAY_STATUS_CHANGE = "edu.vu.isis.ACTION_GATEWAY_STATUS_CHANGE";
+	public static final String AMMO_ACTION_NETLINK_STATUS_CHANGE = "edu.vu.isis.ACTION_NETLINK_STATUS_CHANGE";
 	public static final int LINK_UP = 1;
 	public static final int LINK_DOWN = 2;
 }
