7ef26bbe48313387ec50313a453eec3807a4d532
Matthew Shea <sheamj@ammo6.(none)>
2011-06-10 17:53:47 -0500

    Quasi-functional

--- AmmoLib/src/edu/vu/isis/ammo/api/AmmoDispatcher.java
+++ AmmoLib/src/edu/vu/isis/ammo/api/AmmoDispatcher.java
@@ -183,6 +183,7 @@ public class AmmoDispatcher {
 	 */
 	public boolean post(String mimeType, ContentValues value, Calendar expiration, double worth) 
 	{
+		Log.i("AmmoDispatcher", "post(" + value.toString());
 		Gson gson = new Gson();
         String serializedString = gson.toJson(value);
         return post(mimeType, serializedString, expiration, worth);
@@ -227,7 +228,15 @@ public class AmmoDispatcher {
 	public boolean post(Uri uri, String mimeType, Calendar expiration, double worth) {
 		return post(uri,mimeType,expiration,worth,null);
 	}
+	
+	public boolean post(Uri uri, String mimeType) {
+		return post(uri,mimeType,null,Double.NaN,null);
+	}
+	
+	
 	public boolean post(Uri uri, String mimeType, Calendar expiration, double worth, PendingIntent notice) {
+		
+		Log.i("AmmoDispatcher", "real post [" + uri + "]");
 		// check that the uri is valid
 		if (uri == null) return false;
 		
