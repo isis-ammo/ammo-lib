28099e0f3acf4fb87aacbd9337a20e093baf7e62
Fred Eisele <feisele@isis.vanderbilt.edu>
2011-06-16 10:28:22 -0500

    combined control and status, continued to document java api

--- AmmoLib/src/edu/vu/isis/ammo/api/AmmoRequest.java
+++ AmmoLib/src/edu/vu/isis/ammo/api/AmmoRequest.java
@@ -6,37 +6,6 @@ import android.content.Intent;
 
 abstract public class AmmoRequest implements IAmmoRequest {
   public abstract static class Builder implements IAmmoRequest.Builder {
-     static public Builder make() { return null; }
-  }
-  public abstract static class Event implements IAmmoRequest.Event {
-     static public Color make(Place place, Color color) { return null; }
-  }
-  public abstract static class NoticePendingIntent implements IAmmoRequest.Notice {
-     protected NoticePendingIntent() {}
-     
-     static public IAmmoRequest.Notice make(Event level, PendingIntent action) { 
-        return null; 
-     } 
-     abstract public PendingIntent getAction();
-  }
-  public abstract static class NoticeRunnable implements IAmmoRequest.Notice {
-     protected NoticeRunnable() {}
-     
-     static public IAmmoRequest.Notice make(Event level, Runnable actor) { 
-        return null; 
-     } 
-     abstract public PendingIntent getAction();
-  }
-  public abstract static class NoticeIntent implements IAmmoRequest.Notice {
-     protected NoticeIntent() {}
-
-     static public Notice make(Event level, Intent intent) { 
-        return null; 
-     } 
-     abstract public PendingIntent getAction();
-  }
-     
-  public abstract static class Recipient implements IAmmoRequest.Recipient {
-     static public Recipient make(String name) { return null; }
+     static public Builder getInstance() { return null; }
   }
 }
--- AmmoLib/src/edu/vu/isis/ammo/api/IAmmoRequest.java
+++ AmmoLib/src/edu/vu/isis/ammo/api/IAmmoRequest.java
@@ -23,21 +23,44 @@ public interface IAmmoRequest {
       public IAmmoRequest recover(String uuid);
         public Builder provider(Uri val);
         public Builder payload(String val);
+        public static String DEFAULT_TYPE = "";
         public Builder type(String val);
-        public Builder name(String val);
+
+        public static String DEFAULT_TITLE = "";
+        public Builder title(String val);
+        public static Filter DEFAULT_FILTER = null;
         public Builder filter(Filter val); 
+        public static Query DEFAULT_QUERY = null;
         public Builder query(Query val);
+        public static Query DEFAULT_DOWNSAMPLE = 0;
         public Builder downsample(Downsample val); 
         public Builder downsample(char val); 
+        public static Duration DEFAULT_LOGEVITY = Duration.HOUR;
         public Builder longevity(Duration val);
         public Builder longevity(Calendar val);
+        public static int LOW_PRIORITY = -10;
+        public static int NORMAL_PRIORITY = 0;
+        public static int HIGH_PRIORITY = 10;
+        public static int URGENT = 1000;
+
+        public static int DEFAULT_PRIORITY = NORMAL_PRIORITY;
         public Builder priority(int val);
+        public static int DEFAULT_WORTH = 100;
         public Builder worth(int val);
+        public static int DEFAULT_LIVENESS = 0;
         public Builder liveness(int val);
+        public static int DEFAULT_START = Calendar.NOW;
         public Builder start(Calendar val); 
+        public static int DEFAULT_SCOPE = -1;
         public Builder scope(int val);
+        public static int ANY_RECIPIENT = null;
+
+        public static int DEFAULT_RECIPIENT = ANY_RECIPIENT;
         public Builder recipient(Recipient val);
-        public Builder maxTransmissionRate(int val);
+        public static int UNLIMITED_THROTTLE = -1;
+
+        public static int DEFAULT_THROTTLE = UNLIMITED_THROTTLE;
+        public Builder throttle(int val);
          public Event[] getEvent(); 
          public Event[] cancel(); 
          public Event[] expiration(Duration val);
