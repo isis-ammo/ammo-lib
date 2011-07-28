9eddb8fc6a3eaeb71e5600e2a1353ae1ecb5cdc8
Fred Eisele <feisele@isis.vanderbilt.edu>
2011-07-28 10:51:03 -0500

    refactored constants and types from bulder to ammo request interface

--- AmmoLib/src/edu/vu/isis/ammo/api/IAmmoRequest.java
+++ AmmoLib/src/edu/vu/isis/ammo/api/IAmmoRequest.java
@@ -16,6 +16,68 @@ public interface IAmmoRequest {
    public TimeStamp lastMessage();
    public void resetMetrics(int val);
    public Event[] eventSet(); 
+   public static final Uri DEFAULT_PROVIDER = null;
+   public enum PayloadType { NONE, STR, BYTE, CV };
+   public static final String DEFAULT_PAYLOAD = "";
+   public enum TopicEncoding { STR, OID };
+
+   public static final String DEFAULT_TOPIC = "";
+   public enum ExpireType { ABS, REL };
+
+   public static final TimeInterval UNLIMITED_EXPIRE = 
+         new TimeInterval(TimeInterval.UNLIMITED);
+   public static final TimeInterval DEFAULT_EXPIRE = 
+         new TimeInterval(TimeInterval.Unit.HOUR);
+   public static final int VOLATILE_DURABILITY = 1;
+   public static final int PERSISTENT_DURABILITY = 2;
+
+   public static final int DEFAULT_DURABILITY = PERSISTENT_DURABILITY;
+   public static final int BACKGROUND_PRIORITY = -1000;
+   public static final int LOW_PRIORITY = -10;
+   public static final int NORMAL_PRIORITY = 0;
+   public static final int HIGH_PRIORITY = 10;
+   public static final int URGENT_PRIORITY = 1000;
+
+   public static final int DEFAULT_PRIORITY = NORMAL_PRIORITY;
+   public static final int OLDEST_FIRST_ORDER = 1;
+   public static final int NEWEST_ONLY_ORDER = 2;
+   public static final int NEWEST_FIRST_ORDER = 3;
+
+   public static final int DEFAULT_ORDER = OLDEST_FIRST_ORDER;
+   public static final int DEFAULT_WORTH = 100;
+   public enum StartType { ABS, REL };
+
+   public static final TimeInterval DEFAULT_START = 
+        new TimeInterval(TimeInterval.Unit.MINUTE);
+   public static String[] ALL_PROJECT = null;
+
+   public static String[] DEFAULT_PROJECT = ALL_PROJECT;
+   public static final Query ALL_SELECT = null;
+   public enum SelectType { QUERY, FORM };
+
+   public static final Query DEFAULT_SELECT = ALL_SELECT;
+   public static Form DEFAULT_FORM = null;
+   public static final String NO_FILTER = "";
+
+   public static final String DEFAULT_FILTER = NO_FILTER;
+   public static final int NO_DOWNSAMPLE = 0;
+   public static final int DEFAULT_DOWNSAMPLE = NO_DOWNSAMPLE;
+   public static final Notice DEFAULT_NOTICE = null;
+   public static final Anon ANY_ANON = null;
+   public static final Anon DEFAULT_RECIPIENT = ANY_ANON;
+   public static final Anon DEFAULT_ORIGINATOR = ANY_ANON;
+   public enum DeliveryScope {
+      IMMEDIATE, LOCAL, GLOBAL, RECIPIENT 
+   };
+
+   public static final DeliveryScope DEFAULT_SCOPE = DeliveryScope.GLOBAL;
+   public static final int UNLIMITED_THROTTLE = -1;
+
+   public static final int DEFAULT_THROTTLE = UNLIMITED_THROTTLE;
+   public static final String DEFAULT_UID = "";
+   public static final int DEFAULT_DEPTH = 0;
+   public static final int UN_LIMIT = -1;
+   public static final int DEFAULT_LIMIT = UN_LIMIT;
 
    public interface Builder {
       public IAmmoRequest duplicate();
@@ -28,113 +90,31 @@ public interface IAmmoRequest {
       public IAmmoRequest retrieve() throws RemoteException;
       public IAmmoRequest getInstance(String uuid) throws RemoteException;
         public Builder provider(Uri val);
-
-        public static final Uri DEFAULT_PROVIDER = null;
-
-        public enum PayloadType { NONE, STR, BYTE, CV };
-        public static final String DEFAULT_PAYLOAD = "";
-
         public Builder payload(String val);
         public Builder payload(byte[] val);
         public Builder payload(ContentValues val);
         public Builder payload(AmmoValues val);
-        public enum TopicEncoding { STR, OID };
-
         public Builder topic(String val);
         public Builder topic(Oid val); 
         // \availability{2.0}
-
-        public static final String DEFAULT_TOPIC = "";
-
-        public static final String DEFAULT_UID = "";
         public Builder uid(String val);
-        public enum ExpireType { ABS, REL };
-
         public Builder expire(TimeInterval val);
         public Builder expire(TimeStamp val);
-
-        public static final TimeInterval UNLIMITED_EXPIRE = 
-              new TimeInterval(TimeInterval.UNLIMITED);
-        public static final TimeInterval DEFAULT_EXPIRE = 
-              new TimeInterval(TimeInterval.Unit.HOUR);
-
-        public static final int VOLATILE_DURABILITY = 1;
-        public static final int PERSISTENT_DURABILITY = 2;
-
-        public static final int DEFAULT_DURABILITY = PERSISTENT_DURABILITY;
         public Builder durability(int val);
-        public static final Anon ANY_ANON = null;
-
         public Builder recipient(Anon val);
         public Builder originator(Anon val);
-
-        public static final Anon DEFAULT_RECIPIENT = ANY_ANON;
-        public static final Anon DEFAULT_ORIGINATOR = ANY_ANON;
-
-        public static final int BACKGROUND_PRIORITY = -1000;
-        public static final int LOW_PRIORITY = -10;
-        public static final int NORMAL_PRIORITY = 0;
-        public static final int HIGH_PRIORITY = 10;
-        public static final int URGENT_PRIORITY = 1000;
-
         public Builder priority(int val);
-
-        public static final int DEFAULT_PRIORITY = NORMAL_PRIORITY;
-
-        public static final int OLDEST_FIRST_ORDER = 1;
-        public static final int NEWEST_ONLY_ORDER = 2;
-        public static final int NEWEST_FIRST_ORDER = 3;
-
         public Builder order(int val);
         public Builder order(String[] val);
-
-        public static final int DEFAULT_ORDER = OLDEST_FIRST_ORDER;
-
-        public enum StartType { ABS, REL };
-
+        public Builder worth(int val);
         public Builder start(TimeStamp val); 
         public Builder start(TimeInterval val); 
-
-        public static final TimeInterval DEFAULT_START = 
-             new TimeInterval(TimeInterval.Unit.MINUTE);
-
-        public enum DeliveryScope {
-           IMMEDIATE, LOCAL, GLOBAL, RECIPIENT 
-        };
-
         public Builder scope(DeliveryScope val);
-
-        public static final DeliveryScope DEFAULT_SCOPE = DeliveryScope.GLOBAL;
-        public static final int UNLIMITED_THROTTLE = -1;
-
         public Builder throttle(int val);
-
-        public static final int DEFAULT_THROTTLE = UNLIMITED_THROTTLE;
-
-        public static final String NO_FILTER = "";
-
         public Builder filter(String val); 
-
-        public static final String DEFAULT_FILTER = NO_FILTER;
-
-        public static final int NO_DOWNSAMPLE = 0;
-        public static final int DEFAULT_DOWNSAMPLE = NO_DOWNSAMPLE;
-
         public Builder downsample(int maxSize); 
-        public static String[] ALL_PROJECT = null;
-
         public Builder project(String[] val);
-
-        public static String[] DEFAULT_PROJECT = ALL_PROJECT;
-
-        public static final Query ALL_SELECT = null;
-        public enum SelectType { QUERY, FORM };
-
         public Builder select(Query val);
-
-        public static final Query DEFAULT_SELECT = ALL_SELECT;
-
-        public static Form DEFAULT_FORM = null;
         public Builder select(Form val); 
    }
    public enum Action {
