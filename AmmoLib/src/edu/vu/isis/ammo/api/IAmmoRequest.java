d1c379d31273508612a2109ea3c2e57e5bef554b
Fred Eisele <feisele@isis.vanderbilt.edu>
2011-08-22 12:44:03 -0500

    generated header files

--- AmmoLib/src/edu/vu/isis/ammo/api/IAmmoRequest.java
+++ AmmoLib/src/edu/vu/isis/ammo/api/IAmmoRequest.java
@@ -5,15 +5,15 @@ import java.util.Map;
 
 import android.content.ContentValues;
 import android.net.Uri;
-import android.os.Parcel;
 import android.os.RemoteException;
+import android.os.Parcel;
+
 import edu.vu.isis.ammo.api.type.DeliveryScope;
 import edu.vu.isis.ammo.api.type.Oid;
 import edu.vu.isis.ammo.api.type.TimeInterval;
 import edu.vu.isis.ammo.api.type.TimeStamp;
 
 public interface IAmmoRequest {
-
    public String uuid();  
    public IAmmoRequest replace(IAmmoRequest req) throws RemoteException;
    public IAmmoRequest replace(String uuid) throws RemoteException;
@@ -22,60 +22,60 @@ public interface IAmmoRequest {
    public TimeStamp lastMessage();
    public void resetMetrics(int val);
    public Event[] eventSet(); 
-   public static final Uri PROVIDER_DEFAULT  = null;
-   public static final String PAYLOAD_DEFAULT  = "";
-   
-   public static final String TOPIC_DEFAULT  = "";
+   public static final Uri PROVIDER_DEFAULT = null;
+   public static final String PAYLOAD_DEFAULT = "";
 
-   public static final TimeInterval EXPIRE_UNLIMITED  = 
+   public static final String TOPIC_DEFAULT = "";
+
+   public static final TimeInterval EXPIRE_UNLIMITED = 
          new TimeInterval(TimeInterval.UNLIMITED);
-   public static final TimeInterval EXPIRE_DEFAULT  = 
+   public static final TimeInterval EXPIRE_DEFAULT = 
          new TimeInterval(TimeInterval.Unit.HOUR);
-   public static final int DURABILITY_VOLATILE  = 1;
-   public static final int DURABILITY_PERSISTENT  = 2;
+   public static final int DURABILITY_VOLATILE = 1;
+   public static final int DURABILITY_PERSISTENT = 2;
 
-   public static final int DURABILITY_DEFAULT  = DURABILITY_PERSISTENT; 
-   public static final int PRIORITY_BACKGROUND  = -1000;
-   public static final int PRIORITY_LOW  = -10;
-   public static final int PRIORITY_NORMAL  = 0;
-   public static final int PRIORITY_HIGH  = 10;
-   public static final int PRIORITY_URGENT  = 1000;
+   public static final int DURABILITY_DEFAULT = DURABILITY_PERSISTENT ;
+   public static final int PRIORITY_BACKGROUND = -1000;
+   public static final int PRIORITY_LOW = -10;
+   public static final int PRIORITY_NORMAL = 0;
+   public static final int PRIORITY_HIGH = 10;
+   public static final int PRIORITY_URGENT = 1000;
 
-   public static final int PRIORITY_DEFAULT  = PRIORITY_NORMAL ;
-   public static final int ORDER_OLDEST_FIRST  = 1;
-   public static final int ORDER_NEWEST_ONLY  = 2;
-   public static final int ORDER_NEWEST_FIRST  = 3;
+   public static final int PRIORITY_DEFAULT = PRIORITY_NORMAL ;
+   public static final int ORDER_OLDEST_FIRST = 1;
+   public static final int ORDER_NEWEST_ONLY = 2;
+   public static final int ORDER_NEWEST_FIRST = 3;
 
-   public static final int ORDER_DEFAULT  = ORDER_OLDEST_FIRST ;
-   public static final int WORTH_DEFAULT  = 100;
+   public static final int ORDER_DEFAULT = ORDER_OLDEST_FIRST ;
+   public static final int WORTH_DEFAULT = 100;
 
-   public static final TimeInterval START_DEFAULT  = 
+   public static final TimeInterval START_DEFAULT = 
         new TimeInterval(TimeInterval.Unit.MINUTE);
-   public static String[] PROJECT_ALL  = null;
+   public static String[] PROJECT_ALL = null;
 
-   public static String[] PROJECT_DEFAULT  = PROJECT_ALL; 
-   public static final Query SELECT_ALL  = null;
+   public static String[] PROJECT_DEFAULT = PROJECT_ALL ;
+   public static final Query SELECT_ALL = null;
 
-   public static final Query SELECT_DEFAULT  = SELECT_ALL ;
-   public static Form FORM_DEFAULT  = null;
-   public static final String FILTER_NO  = "";
+   public static final Query SELECT_DEFAULT = SELECT_ALL ;
+   public static Form FORM_DEFAULT = null;
+   public static final String FILTER_NO = "";
 
-   public static final String FILTER_DEFAULT  = FILTER_NO ;
-   public static final int DOWNSAMPLE_NO  = 0;
-   public static final int DOWNSAMPLE_DEFAULT  = DOWNSAMPLE_NO ;
-   public static final Notice NOTICE_DEFAULT  = null;
-   public static final IAnon ANON_ANY  = null;
-   public static final IAnon RECIPIENT_DEFAULT  = ANON_ANY ;
-   public static final IAnon ORIGINATOR_DEFAULT  = ANON_ANY ;
+   public static final String FILTER_DEFAULT = FILTER_NO ;
+   public static final int DOWNSAMPLE_NO = 0;
+   public static final int DOWNSAMPLE_DEFAULT = DOWNSAMPLE_NO ;
+   public static final Notice NOTICE_DEFAULT = null;
+   public static final IAnon ANON_ANY = null;
+   public static final IAnon RECIPIENT_DEFAULT = ANON_ANY ;
+   public static final IAnon ORIGINATOR_DEFAULT = ANON_ANY ;
 
-   public static final DeliveryScope SCOPE_DEFAULT  = DeliveryScope.GLOBAL;
-   public static final int THROTTLE_UNLIMITED  = -1;
+   public static final DeliveryScope SCOPE_DEFAULT = DeliveryScope.GLOBAL;
+   public static final int THROTTLE_UNLIMITED = -1;
 
-   public static final int THROTTLE_DEFAULT  = THROTTLE_UNLIMITED ;
-   public static final String UID_DEFAULT  = "";
-   public static final int DEPTH_DEFAULT  = 0;
-   public static final int LIMIT_UN  = -1;
-   public static final int LIMIT_DEFAULT  = LIMIT_UN ;
+   public static final int THROTTLE_DEFAULT = THROTTLE_UNLIMITED ;
+   public static final String UID_DEFAULT = "";
+   public static final int DEPTH_DEFAULT = 0;
+   public static final int LIMIT_NONE = -1;
+   public static final int LIMIT_DEFAULT = LIMIT_NONE ;
 
    public interface Builder {
       public IAmmoRequest duplicate() throws RemoteException;
@@ -100,9 +100,9 @@ public interface IAmmoRequest {
         public Builder expire(TimeStamp val);
         public Builder durability(int val);
         public Builder recipient(IAnon val);
-        public Builder recipient(String val); 
+        public Builder recipient(String val);
         public Builder originator(IAnon val);
-        public Builder originator(String val); 
+        public Builder originator(String val);
         public Builder priority(int val);
         public Builder order(int val);
         public Builder order(String[] val);
@@ -115,8 +115,7 @@ public interface IAmmoRequest {
         public Builder downsample(int maxSize); 
         public Builder project(String[] val);
         public Builder select(Query val);
-        public Builder select(Form val);
-		
+        public Builder select(Form val); 
    }
    public enum Action {
      POSTAL, DIRECTED_POSTAL, PUBLISH, RETRIEVAL, SUBSCRIBE, DIRECTED_SUBSCRIBE;
@@ -135,13 +134,12 @@ public interface IAmmoRequest {
          }
      }
 
-	static public void writeToParcel(Parcel dest, Action action) {
-		dest.writeInt(action.ordinal());
-	}
-	static public Action getInstance(Parcel in) { 
-		return Action.values()[in.readInt()];
-	}
-
+     static public void writeToParcel(Parcel dest, Action action) {
+        dest.writeInt(action.ordinal());
+     }
+     static public Action getInstance(Parcel in) { 
+        return Action.values()[in.readInt()];
+     }
    };
 
    public interface IAnon {
@@ -168,7 +166,6 @@ public interface IAmmoRequest {
       DELIVERED, COMPLETED 
    };
    public enum DeliveryState { SUCCESS, FAIL,  UNKNOWN, REJECTED };
-   
    public interface Event {
       public DeliveryProgress progress();
       public Event progress(DeliveryProgress val);
@@ -176,7 +173,6 @@ public interface IAmmoRequest {
       public DeliveryState state();
       public Event state(DeliveryState val);
    }
-   
    public interface Notice {
       public Event target();
       public Notice target(Event val);
@@ -188,4 +184,5 @@ public interface IAmmoRequest {
       public Object action();
       public Notice action(Object val);
    }
+
 }
